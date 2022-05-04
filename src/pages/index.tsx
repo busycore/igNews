95fc015570921b9601499e16f6567d194de4e2f2
Matheus Vargem <matheusvrgm@gmail.com>
2022-05-03 21:48:21 -0300

    Static props render for the price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetServerSideProps, GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function Home() {
+const REVALIDATE_TIME_SECONDS = 60 * 60 * 24; //24 hours
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    price: number;
+  };
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <Head>
@@ -17,12 +28,38 @@ export default function Home() {
           </h1>
           <p>
             Get access to all posts <br />
-            <span> for $9.99 month</span>
+            <span> for {props.product.price} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={props.product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="girl-coding" />
       </main>
     </>
   );
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve("price_1KvV6OCNTTRyL0XV02V2n54D");
+
+//   const product = {
+//     priceId: price.id,
+//     price: new Intl.NumberFormat("en-US", {
+//       style: "currency",
+//       currency: "USD",
+//     }).format(price.unit_amount / 100),
+//   };
+//   return { props: { product } };
+// };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KvV6OCNTTRyL0XV02V2n54D");
+
+  const product = {
+    priceId: price.id,
+    price: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return { props: { product }, revalidate: REVALIDATE_TIME_SECONDS };
+};
