7be1f9577d420862de213843eb34fc711600ff64
Matheus Vargem <matheusvrgm@gmail.com>
2022-05-10 23:15:12 -0300

    Sigin /fauna / stripe payment

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,26 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = useSession().data;
   return isUserLoggedIn ? (
-    <button type="button" className={styles.SignInButton}>
+    <button
+      type="button"
+      className={styles.SignInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04de61" />
-      busycore
+      {isUserLoggedIn.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.SignInButton}>
+    <button
+      type="button"
+      className={styles.SignInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign In with Github
     </button>
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +8,33 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton(props: SubscribeButtonProps) {
+  const session = useSession().data;
+
+  async function createSubscribeSection() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      console.log(response);
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
   return (
-    <button className={styles.subscribeButton} type="button">
+    <button
+      className={styles.subscribeButton}
+      type="button"
+      onClick={() => createSubscribeSection()}
+    >
       Subscribe now
     </button>
   );
